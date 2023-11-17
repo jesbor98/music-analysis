2300dd7db80fb21e0157d6f8c1c89a55f1d5197f
jesbor98 <jessicalborg@gmail.com>
2023-11-17 14:22:57 +0100

    data represented in confusion matrix

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@
 import numpy as np
 import pandas as pd
 import sklearn
-import matplotlib
-import seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
 import csv
 
 
@@ -25,6 +25,25 @@ df.head()
 # Check for missing data again to verify that all null values have been replaced
 missing_data = df.isnull().sum()
 df.head()
+
+
+# Select the relevant columns for the heatmap
+features = df[['valence_%', 'energy_%', 'acousticness_%', 'danceability_%', 'instrumentalness_%', 'liveness_%', 'speechiness_%', 'bpm']]
+
+# Compute the correlation matrix
+correlation_matrix = features.corr()
+
+# Create a heatmap
+plt.figure(figsize=(10, 8))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=.5)
+plt.title('Correlation Heatmap of Song Characteristics')
+plt.show()
+
+
+
+
+
+
 # class DataReader:
 #     def __init__(self, file_path):
 #         self.file_path = file_path
