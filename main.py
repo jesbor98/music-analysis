7e5bc2b854acee1f7c483191da7a4fb4cfe31889
meyzenn <amanda.enhorning@hotmail.se>
2023-12-03 13:21:59 +0100

    added training and test set, prediction of test set based on the dtc

--- main.py
+++ main.py
@@ -37,6 +37,8 @@ print("Attribute 1 primarily represented by PC1: " + attributes_for_PC1[0])
 print("Attribute 1 primarily represented by PC2: " + attributes_for_PC2[0])
 print("Attribute 2 primarily represented by PC1: " + attributes_for_PC1[1])
 print("Attribute 2 primarily represented by PC2: " + attributes_for_PC2[1])
+print("Attribute 3 primarily represented by PC1: " + attributes_for_PC1[2])
+print("Attribute 3 primarily represented by PC2: " + attributes_for_PC2[2])
 
 scatter_pc1_pc2 = plt.figure()
 plt.scatter(Z[:, 0], Z[:, 1])
@@ -48,13 +50,18 @@ plt.show()
 #-----------------------------
 
 #-----------Decision Tree-----------------
-dtc_attr = ['valence_%', 'acousticness_%', 'energy_%']
-X_dtc_attr = df[dtc_attr]
-y_dtc_attr = df['mode'].ravel()
+split_percentage = 0.7
+split_index = int(len(df) * split_percentage)
+
+df_train = df.iloc[:split_index]
+
+dtc_attr = ['valence_%', 'acousticness_%', 'energy_%', 'danceability_%']
+X_dtc_attr = df_train[dtc_attr]
+y_dtc_attr = df_train['mode'].ravel()
 
 feature_names_list = X_dtc_attr.columns.tolist()
 
-mode_counts = df['mode'].value_counts()
+mode_counts = df_train['mode'].value_counts()
 mode_counts = mode_counts.sort_values(ascending=False)
 print(mode_counts)
 
@@ -65,4 +72,65 @@ plt.figure(figsize=(100, 100))
 plot_tree(dtc, feature_names=feature_names_list, class_names=['Major', 'Minor'], filled=True, rounded=True, impurity=True, fontsize=8)
 plt.show()
 
-#------------------------------------
+#------------------------------------------------
+
+#---------Confusion Matrix------------
+df_test = df.iloc[split_index:]
+
+true_positive = 0
+true_negative = 0
+false_positive = 0
+false_negative = 0
+
+# Keep track of sampled songs to avoid duplicates
+sampled_songs = set()
+
+for _ in range(50):
+    # Randomly sample a row from df_test that hasn't been sampled before
+    remaining_samples = df_test[~df_test.index.isin(sampled_songs)]
+    if remaining_samples.empty:
+        break
+    random_sample = remaining_samples.sample(n=1)
+
+    # Extract attributes for prediction
+    X_random_sample = random_sample[dtc_attr]
+
+    # Predict the mode for the random sample
+    predicted_mode = dtc.predict(X_random_sample)[0]
+
+    # Update confusion matrix variables
+    actual_mode = random_sample['mode'].values[0]
+    if actual_mode == 'Major' and predicted_mode == 'Major':
+        true_positive += 1
+    elif actual_mode == 'Minor' and predicted_mode == 'Minor':
+        true_negative += 1
+    elif actual_mode == 'Major' and predicted_mode == 'Minor':
+        false_negative += 1
+    elif actual_mode == 'Minor' and predicted_mode == 'Major':
+        false_positive += 1
+
+    # Add the sampled song to the set
+    sampled_songs.add(random_sample.index[0])
+
+# Create and print confusion matrix
+conf_matrix = pd.DataFrame([[true_positive, false_negative], [false_positive, true_negative]],
+                            index=['Actual Major', 'Actual Minor'],
+                            columns=['Predicted Major', 'Predicted Minor'])
+print("\nConfusion Matrix:")
+print(conf_matrix)
+
+# Plot confusion matrix
+fig, ax = plt.subplots()
+cax = ax.matshow(conf_matrix, cmap='Blues')
+plt.colorbar(cax)
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.xticks([0, 1], conf_matrix.columns)
+plt.yticks([0, 1], conf_matrix.index)
+for i in range(2):
+    for j in range(2):
+        plt.text(j, i, str(conf_matrix.iloc[i, j]), ha='center', va='center', color='black', fontsize=12)
+plt.show()
+# SKRIV KOMMENTAR HÄR ANGÅENDE ATT VI HADE PROBLEM MED CONF MATRIXEN
+
+#--------------------------------------
